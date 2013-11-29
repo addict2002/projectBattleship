e9f81ccbb8fd7ce91e4d39540c3b42a2670e0ee3
andricapol <al_capol@hotmail.com>
2013-11-29 10:39:22 +0100

    hhjgkjghkhj

---
+++ Battleship/src/battleship/oponent/Oponent.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package battleship.oponent;
+
+/**
+ *
+ * @author Andri
+ */
+public class Oponent implements IOponent{
+    String name;
+
+    @Override
+    public boolean sendBomb() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean receiveBomb() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
