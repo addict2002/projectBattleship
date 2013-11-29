235b05584c9a2c9845783dd92d1dc92feba7d487
Sandro <Sandro@10.3.112.252>
2013-11-29 11:33:53 +0100

    jetzt wird commited

--- Battleship/src/battleship/gui/modeselector.java
+++ Battleship/src/battleship/gui/modeselector.java
@@ -4,17 +4,34 @@
  */
 package battleship.gui;
 import javax.swing.*;
-
+import java.awt.*;
 /**
  *
  * @author Sandro
  */
 public class modeselector extends JFrame {
-    private JLabel welcome;
-    private JLabel gd; 
-            
-            
+
+    private JLabel welcome = new JLabel("Welcome");
+    private JLabel choose = new JLabel("Choose your Mode");
+    private JPanel panel;
+    private String[] modes = { "Singleplayer", "Multiplayer"};
+    private JComboBox mode = new JComboBox(modes);
+    
     public modeselector(){
-        gd = new JLabel();
+        super("Welcome");
+        setSize(600,600);
+        setLocation(300,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setLayout(new GridLayout(5,1));
+        welcome.setHorizontalAlignment(JLabel.CENTER);
+        add(welcome);
+        add(choose);
+        add(mode);
+        setVisible(true);
+    }
+     public static void main(String[] args)
+    {
+        modeselector g = new modeselector();
     }
 }
