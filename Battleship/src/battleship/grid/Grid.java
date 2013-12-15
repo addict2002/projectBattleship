614ce3b20b887e3577157ae165f242dca9bdde6a
addict2002 <bruno.schriber@stud.hslu.ch>
2013-12-15 23:05:34 +0100

    Liste hinzugefügt, um die Anzahl Schiffe zu kontrolliern

--- Battleship/src/battleship/grid/Grid.java
+++ Battleship/src/battleship/grid/Grid.java
@@ -6,12 +6,17 @@
 
 package battleship.grid;
 
+import java.util.Iterator;
+import java.util.ArrayList;
+
 /**
  *
  * @author ceeedi
  */
 public class Grid {
     private final Field[][] grid;
+    private ArrayList<Ship> shipList = new ArrayList<Ship>();
+    private final int MAX_SHIP_COUNT = 10;
     
     public Grid(int xSize, int ySize){
         grid = new Field[xSize][ySize];        
@@ -23,21 +28,44 @@ public class Grid {
         
         size=ship.getShipSize();
         
-        //add vertiacal ship
-        if(direction==0){
-            for(i=0;i<size;i++){
-                grid[xCord][yCord].ship=ship;
-                yCord++;
+        /*change von bruno */
+        /* das shiff wird zuerst in die shipList eingetragen, da wird geprüft das nur die maximale Anzahl schiffe plaziert werden kann */
+        
+        if(addShipToList(ship))
+        {
+            //add vertiacal ship
+            if(direction==0){
+                for(i=0;i<size;i++){
+                    grid[xCord][yCord].ship=ship;
+                    yCord++;
+                }
+            }
+            //add horizontal ship
+            else{
+                for(i=0;i<size;i++){
+                    grid[xCord][yCord].ship=ship;
+                    xCord++;
+                }
             }
         }
-        //add horizontal ship
-        else{
-            for(i=0;i<size;i++){
-                grid[xCord][yCord].ship=ship;
-                xCord++;
-            }
-        }
+        
     }
+  
+    /*change von bruno */
+    /* funktion welche shiffe in die Liste einträgt, maximal MAX_SHIP_COUNT */
+    
+    //adds ships to shipList
+    private boolean addShipToList(Ship ship)
+    {
+        boolean isShipAdded = false;
+        if(shipList.size() > MAX_SHIP_COUNT)    
+        {
+            shipList.add(ship);
+            isShipAdded = true;
+        }
+        return isShipAdded;
+    }
+    
     /**
      * @param args the command line arguments
      */
