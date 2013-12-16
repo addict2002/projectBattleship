8dee5310b48975e9e200303305a59921e0704cc7
addict2002 <bruno.schriber@stud.hslu.ch>
2013-12-16 22:22:11 +0100

    Liste hinzugefügt, um die Anzahl Schiffe zu kontrolliern

--- Battleship/src/battleship/Battleship.java
+++ Battleship/src/battleship/Battleship.java
@@ -6,6 +6,12 @@
 
 package battleship;
 
+import battleship.engine.Engine;
+import battleship.gui.ModeSelector;
+import battleship.gui.Play;
+import battleship.gui.ShipPlacement;
+import java.io.IOException;
+
 /**
  *
  * @author bruno
@@ -15,9 +21,37 @@ public class Battleship {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    
+    public Engine gameEngine;
+    
+    private ModeSelector screenModeSelector;
+    private ShipPlacement screenShipPlacement;
+    private Play screenPlay;
+    
+    public Battleship()
+    {
+        this.screenModeSelector = new ModeSelector(this);
+    }
+    
+    public void createHostEngine() throws IOException
+    {
+        this.gameEngine = Engine.createHostEnginge();
+        this.screenModeSelector.setVisible(false);
+        
+        this.screenShipPlacement = new ShipPlacement(this);
+    }
+    
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
         //Some Change
+       
+       Battleship game = new Battleship();
+       //game.screenModeSelector.setVisible(true);
+       
+       // Engine gameEngine = Engine.createHostEnginge();
+       
+       //  System.out.println("Current State of the Game: " + gameEngine.currentGameState);
+       
         
     }
     
